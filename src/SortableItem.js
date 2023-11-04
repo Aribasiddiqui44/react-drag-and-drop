4a961c6623ccb314a037dd9a02b27076052e9fce
Aribasiddiqui44 <aribasiddi4@gmail.com>
2023-11-04 16:02:32 +0500

    created drag and drop application

---
+++ src/SortableItem.js
@@ -0,0 +1,25 @@
+import {useSortable} from "@dnd-kit/sortable"
+import {CSS} from "@dnd-kit/utilities"
+import { Card } from "react-bootstrap"
+
+
+export function SortableItem(props) {
+    const{
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition
+    } = useSortable({id: props.id})
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition
+    }
+
+    return(
+        <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+            <Card body className="m-3" style={{"background-color":"steelblue", "color":"white", "font-size":"20px"}}>{props.id}</Card>
+        </div>
+    )
+}
